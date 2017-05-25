562109ed79174c6672f737965055e12b1f702a49
pevsegneev <deadswallow@gmail.com>
2017-05-25 18:08:21 +0300

    Add observer

---
+++ src/lessons/js-patterns/behavioral/observer/sandbox.js
@@ -0,0 +1,95 @@
+function extend(obj, extension) {
+    for (var key in extension) {
+        obj[key] = extension[key];
+    }
+}
+
+function ObserverList() {
+    this.observerList = [];
+}
+
+ObserverList.prototype.add = function (obj) {
+    return this.observerList.push(obj);
+};
+
+ObserverList.prototype.count = function () {
+    return this.observerList.length;
+};
+
+ObserverList.prototype.get = function (index) {
+    if (index > -1 && index < this.observerList.length) {
+        return this.observerList[index];
+    }
+};
+
+ObserverList.prototype.indexOf = function (obj, startIndex) {
+    var i = startIndex;
+
+    while (i < this.observerList.length) {
+        if (this.observerList[i] === obj) {
+            return i;
+        }
+        i++;
+    }
+
+    return -1;
+
+};
+
+ObserverList.prototype.removeAt = function (index) {
+    return this.observerList.splice(index, 1);
+};
+
+//Define Subject object ------------------------------------
+function Subject() {
+    this.observers = new ObserverList();
+}
+
+Subject.prototype.addObserver = function (observer) {
+    this.observers.add(observer);
+};
+
+Subject.prototype.removeObserver = function (observer) {
+    this.observers.removeAt(this.observers.indexOf(observer), 0);
+};
+
+Subject.prototype.notify = function (context) {
+    var observerCount = this.observers.count();
+    for (var i = 0; i < observerCount; i++) {
+        this.observers.get(i).update(context);
+    }
+};
+
+function Observer() {
+    this.update = function () {
+    };
+}
+
+const addBtn = sandbox.createElement("button");
+addBtn.innerHTML = 'Add new observer';
+const controlCheckbox = sandbox.createElement("input");
+controlCheckbox.type = 'checkbox';
+const container = sandbox.createElement("div");
+
+extend(controlCheckbox, new Subject());
+
+controlCheckbox.onclick = function () {
+    controlCheckbox.notify(controlCheckbox.checked);
+};
+
+addBtn.onclick = addNewObserver;
+
+function addNewObserver() {
+    const check = sandbox.createElement('input');
+    check.type = 'checkbox';
+
+    extend(check, new Observer(), );
+
+    check.update = function (value) {
+        this.checked = value;
+    };
+
+    controlCheckbox.addObserver(check);
+    container.appendChild(check);
+
+}
